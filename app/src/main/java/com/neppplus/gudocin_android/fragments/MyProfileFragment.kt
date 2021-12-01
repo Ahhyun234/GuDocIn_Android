81006c14e5dc9a3a47b1b4653659de510791c7f3
ujin-Bang <qkddnwls14@gmail.com>
2021-12-01 14:36:18 +0900

    구매내역 엑티비티의 xml 구조 프래그먼트 2개를 텝레이아웃 / 뷰페이저 표시 예정

--- app/src/main/java/com/neppplus/gudocin_android/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/neppplus/gudocin_android/fragments/MyProfileFragment.kt
@@ -34,6 +34,13 @@ class MyProfileFragment : BaseFragment() {
 
     override fun setupEvents() {
 
+        binding.txtMyProductPurnchaseList.setOnClickListener {
+
+            val myIntent = Intent(mContext, MyProductPurchaseListActivity::class.java)
+
+            startActivity(myIntent)
+        }
+
         binding.btnSaveMoney.setOnClickListener {
 
             val myIntent =  Intent( mContext, SaveMonyMyActivity::class.java )
